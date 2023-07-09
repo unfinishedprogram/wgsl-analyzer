e6244cbe26bae865b58c37c0099197d4f9a4c584
Noah Labrecque <noah.labrecque@gmail.com>
2023-07-09 10:54:40 -0400

    Use codespan to simplify

--- wgsl-language-server/src/pretty_error/handle_expression.rs
+++ wgsl-language-server/src/pretty_error/handle_expression.rs
@@ -1,14 +1,9 @@
 use naga::{Expression, Handle};
 
-use super::error_context::{ContextErrorPrint, LabelContext};
+use super::error_context::{code_provider::CodeProvider, ContextErrorPrint, LabelContext};
 
 impl ContextErrorPrint for Handle<Expression> {
     fn print(&self, context: &LabelContext) -> String {
-        let expression_range = context.error_context.module.functions[context.function]
-            .expressions
-            .get_span(*self)
-            .to_range()
-            .unwrap_or_default();
-        context.error_context.code[expression_range].into()
+        context.code_span(*self).into()
     }
 }
