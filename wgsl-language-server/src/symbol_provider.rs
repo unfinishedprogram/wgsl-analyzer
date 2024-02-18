2a49f1a97d0207bb55b3492c2a5f8f3f6dc969cb
Noah Labrecque <noah.labrecque@gmail.com>
2024-02-18 16:18:11 -0500

    better ergo

--- wgsl-language-server/src/symbol_provider.rs
+++ wgsl-language-server/src/symbol_provider.rs
@@ -1,6 +1,8 @@
+use std::ops::{Index, Range};
+
 use lsp_types::{DocumentSymbol, SymbolKind};
 use wgsl_ast::front::{
-    ast::statement::declaration::{Declaration, StructMember},
+    ast::statement::declaration::{self, Declaration, Struct, StructMember},
     span::Spanned,
 };
 
@@ -22,68 +24,92 @@ impl SymbolProvider for TrackedDocument {
             .declarations()
             .into_iter()
             .filter_map(|Spanned { span, inner }| match inner {
-                Declaration::Variable { ident, .. } => Some(DocumentSymbol {
-                    name: ident.inner.0,
-                    kind: SymbolKind::Variable,
-                    detail: None,
-                    range: lsp_range_from_char_span(&self.content, span.into()),
-                    selection_range: lsp_range_from_char_span(&self.content, ident.span.into()),
-                    children: None,
-                    deprecated: None,
-                    tags: None,
-                }),
-                Declaration::Struct { ident, members, .. } => {
-                    let children = Some(
-                        members
-                            .iter()
-                            .map(
-                                |StructMember {
-                                     attributes,
-                                     ident,
-                                     value,
-                                 }| DocumentSymbol {
-                                    name: ident.inner.clone(),
-                                    kind: SymbolKind::Field,
-                                    detail: Some(format!("{value:?}")),
-                                    range: lsp_range_from_char_span(
-                                        &self.content,
-                                        ident.span.into(),
-                                    ),
-                                    children: None,
-                                    deprecated: None,
-                                    tags: None,
-                                    selection_range: lsp_range_from_char_span(
-                                        &self.content,
-                                        ident.span.into(),
-                                    ),
-                                },
-                            )
-                            .collect(),
-                    );
-
-                    Some(DocumentSymbol {
-                        name: ident.inner,
-                        kind: SymbolKind::Struct,
-                        detail: None,
-                        range: lsp_range_from_char_span(&self.content, span.into()),
-                        selection_range: lsp_range_from_char_span(&self.content, ident.span.into()),
-                        children,
-                        deprecated: None,
-                        tags: None,
-                    })
+                Declaration::Variable(v) => {
+                    Some(v.into_document_symbol(&self.content, span.into()))
                 }
-                Declaration::Function { ident, .. } => Some(DocumentSymbol {
-                    name: ident.inner,
-                    kind: SymbolKind::Function,
-                    detail: None,
-                    range: lsp_range_from_char_span(&self.content, span.into()),
-                    selection_range: lsp_range_from_char_span(&self.content, ident.span.into()),
-                    children: None,
-                    deprecated: None,
-                    tags: None,
-                }),
+                Declaration::Struct(s) => Some(s.into_document_symbol(&self.content, span.into())),
+                Declaration::Function(f) => {
+                    Some(f.into_document_symbol(&self.content, span.into()))
+                }
                 _ => None,
             })
             .collect()
+    }
+}
+
+pub trait IntoDocumentSymbol {
+    fn into_document_symbol(self, source: &str, span: Range<usize>) -> DocumentSymbol;
+}
+
+impl IntoDocumentSymbol for declaration::Function {
+    #[allow(deprecated)]
+    fn into_document_symbol(self, source: &str, span: Range<usize>) -> DocumentSymbol {
+        DocumentSymbol {
+            name: self.ident.inner,
+            kind: SymbolKind::Function,
+            detail: None,
+            range: lsp_range_from_char_span(source, span),
+            selection_range: lsp_range_from_char_span(source, self.ident.span.into()),
+            children: None,
+            tags: None,
+            deprecated: None,
+        }
+    }
+}
+
+impl IntoDocumentSymbol for StructMember {
+    #[allow(deprecated)]
+    fn into_document_symbol(self, source: &str, span: Range<usize>) -> DocumentSymbol {
+        DocumentSymbol {
+            name: self.ident.inner,
+            kind: SymbolKind::Field,
+            detail: Some(source[self.value].to_owned()),
+            range: lsp_range_from_char_span(source, span),
+            children: None,
+            deprecated: None,
+            tags: None,
+            selection_range: lsp_range_from_char_span(source, self.ident.span.into()),
+        }
+    }
+}
+
+impl IntoDocumentSymbol for Struct {
+    #[allow(deprecated)]
+    fn into_document_symbol(self, source: &str, span: Range<usize>) -> DocumentSymbol {
+        let members = self.members.clone();
+
+        let children = Some(
+            members
+                .into_iter()
+                .map(|s| s.inner.into_document_symbol(source, s.span.into()))
+                .collect(),
+        );
+
+        DocumentSymbol {
+            name: self.ident.inner,
+            kind: SymbolKind::Struct,
+            detail: None,
+            range: lsp_range_from_char_span(source, span),
+            selection_range: lsp_range_from_char_span(source, self.ident.span.into()),
+            children,
+            deprecated: None,
+            tags: None,
+        }
+    }
+}
+
+impl IntoDocumentSymbol for declaration::Variable {
+    #[allow(deprecated)]
+    fn into_document_symbol(self, source: &str, span: Range<usize>) -> DocumentSymbol {
+        DocumentSymbol {
+            name: self.ident.inner.0.clone(),
+            kind: SymbolKind::Variable,
+            detail: self.ident.1.as_ref().map(|ty| source[ty].to_owned()),
+            range: lsp_range_from_char_span(source, span),
+            selection_range: lsp_range_from_char_span(source, self.ident.span.into()),
+            children: None,
+            deprecated: None,
+            tags: None,
+        }
     }
 }
